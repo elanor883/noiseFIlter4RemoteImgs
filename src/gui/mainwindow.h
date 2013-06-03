9b576cf5a25bf18fe536230669b5a2495182175e
Elanor <elanor883@gmail.com>
2013-06-03 15:02:37 +0300

    Update mainwindow.h

--- src/gui/mainwindow.h
+++ src/gui/mainwindow.h
@@ -24,22 +24,18 @@ public:
 
 private slots:
     void open();
-    void zoomIn();
-    void zoomOut();
     void applyFilter();
-    void scaleImage(double factor);
-    void handleSelectionChanged(int);
-    void adjustScrollBar(QScrollBar *scrollBar, double factor);
     void display(cv::Mat* img);
+    void saveImage();
 
 
 private:
     Ui::MainWindow *ui;
 
-    double scaleFactor;
     int kernelSize;
     int kernelType;
     cv::Mat img;
+    cv::Mat* output ;
 
 };
 
